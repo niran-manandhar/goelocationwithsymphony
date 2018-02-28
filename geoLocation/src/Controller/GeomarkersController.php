3eac78246d8a782d7f8ea338c1336822709d67ac
niran <notsokool.niran@gmail.com>
2018-02-28 14:43:04 +0200

    save and view
    save and view

--- geoLocation/src/Controller/GeomarkersController.php
+++ geoLocation/src/Controller/GeomarkersController.php
@@ -12,13 +12,15 @@ class GeomarkersController extends Controller
      */
     public function index()
     {
+  if (isset($_POST['submitLocation'])) {
     $em = $this->getDoctrine()->getManager();
 
        $loc = new Geomarkers();
-       $loc->setTitle('Kathmandu');
-       $loc->setLng(19.99);
-       $loc->setLat(19.99);
-       $loc->setAddress('My home town');
+       $loc->setTitle( urlencode($_POST["location_value"]));
+       $loc->setLng(urlencode($_POST["longField_searched"]));
+
+       $loc->setLat(urlencode($_POST["latField_searched"]));
+       $loc->setAddress(urlencode($_POST["address_searched"]));
        $loc->setImage('1.jpg');
 
        // tell Doctrine you want to (eventually) save the Product (no queries yet)
@@ -26,24 +28,123 @@ class GeomarkersController extends Controller
 
        // actually executes the queries (i.e. the INSERT query)
        $em->flush();
-return $this->render('geomarkers/index.html.twig', ['location' => $loc]);
+
+       $address = urlencode($_POST["address_searched"]);
+       $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyC2ssmB4OYp3klzfoEhQFrbIL57NbOcnK4";
+       $resp_json = file_get_contents($url);
+       $resp = json_decode($resp_json, true);
+       if ($resp['status']=='OK') {
+         $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+         $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+         $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+         if ($lati && $longi && $formatted_address) {
+           return $this->render('geomarkers/index.html.twig', array(
+                 'longitude' => $longi, 'latitude' => $lati, 'formatted_address'=> $formatted_address,'datasaved'=> true
+           ));
+         } else {
+           return false;
+         }
+       }
+       else {
+         echo "<strong>ERROR: {$resp['status']}</strong>";
+         return false;
+       }
+
+
+
+  }
+    else if (isset($_GET['submitForm'])) {
+
+          $address = urlencode($_GET["address"]);
+          $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyC2ssmB4OYp3klzfoEhQFrbIL57NbOcnK4";
+          $resp_json = file_get_contents($url);
+          $resp = json_decode($resp_json, true);
+          if ($resp['status']=='OK') {
+            $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+            $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+            $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+            if ($lati && $longi && $formatted_address) {
+              return $this->render('geomarkers/index.html.twig', array(
+                    'longitude' => $longi, 'latitude' => $lati, 'formatted_address'=> $formatted_address
+              ));
+            } else {
+              return false;
+            }
+          }
+          else {
+            echo "<strong>ERROR: {$resp['status']}</strong>";
+            return false;
+          }
+        }
+        else {
+          return $this->render('geomarkers/index.html.twig');
+        }
+
+
+
+
+
       // return new Response('Saved new product with id '.$loc->getId());
     }
 
     /**
- * @Route("/geomarkers/{id}", name="geomarkers_show")
+ * @Route("/geomarkers/show/{id}", name="geomarkers_show")
  */
 public function showAction($id)
 {
 
 $repository = $this->getDoctrine()->getRepository(Geomarkers::class);
 $geomarkers = $repository->find($id);
-
+$address=$geomarkers->getAddress();
     if (!$geomarkers) {
         throw $this->createNotFoundException(
             'No product found for id '.$id
         );
     }
-    return $this->render('geomarkers/get.html.twig', ['location' => $geomarkers]);
+
+
+
+    $url = "https://maps.googleapis.com/maps/api/geocode/json?address={$address}&key=AIzaSyC2ssmB4OYp3klzfoEhQFrbIL57NbOcnK4";
+    $resp_json = file_get_contents($url);
+    $resp = json_decode($resp_json, true);
+    if ($resp['status']=='OK') {
+      $lati = isset($resp['results'][0]['geometry']['location']['lat']) ? $resp['results'][0]['geometry']['location']['lat'] : "";
+      $longi = isset($resp['results'][0]['geometry']['location']['lng']) ? $resp['results'][0]['geometry']['location']['lng'] : "";
+      $formatted_address = isset($resp['results'][0]['formatted_address']) ? $resp['results'][0]['formatted_address'] : "";
+      if ($lati && $longi && $formatted_address) {
+        return $this->render('geomarkers/get.html.twig',['location' => $geomarkers]);
+      } else {
+        return false;
+      }
+    }
+    else {
+      echo "<strong>ERROR: {$resp['status']}</strong>";
+      return false;
+    }
+
+
+
+}
+
+/**
+* @Route("/geomarkers/showall", name="showall")
+*/
+public function showAll()
+{
+
+$repository = $this->getDoctrine()->getRepository(Geomarkers::class);
+$geomarkers = $repository->findAll();
+
+if (!$geomarkers) {
+    throw $this->createNotFoundException(
+        'No product found for id '.$id
+    );
+}
+
+
+
+
+    return $this->render('geomarkers/showall.html.twig',['location' => $geomarkers]);
+
 }
 }
